fcb4a85a713919818cddb2a3d03e41a32ccc98e3
AnthonyCampbell208 <78286293+AnthonyCampbell208@users.noreply.github.com>
2022-07-13 17:24:49 -0400

    .

--- app.py
+++ app.py
@@ -8,24 +8,11 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 # routes
-@app.route('/', methods=['POST'])
-
+@app.route('/', methods=["GET",'POST'])
 def predict():
     # get data
     data = request.get_json(force=True)
-
-    # # convert data into dataframe
-    # data.update((x, [y]) for x, y in data.items())
-    # data_df = pd.DataFrame.from_dict(data)
-
-    # # predictions
-    # result = model.predict(data_df)
-
-    # # send back to browser
-    # output = {'results': int(result[0])}
-
-    # return data
-    return jsonify(data)
+    return "Hi fuck us"
 
 if __name__ == '__main__':
     app.run()
